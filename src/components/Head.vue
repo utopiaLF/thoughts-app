a5f5c7f769ffc6ba8a0bd59037c5645c58724d40
Johnny <jahongirhakimov677@gmail.com>
2025-07-08 23:11:11 +0500

    🔗 Fix logo link path

--- src/components/Head.vue
+++ src/components/Head.vue
@@ -1,13 +1,18 @@
 <template>
     <div class="head">
         <i class="bi bi-list-ul openBarBtn" @click="$emit('openNavbar')"></i>
-        <a href="/">Thoughts App</a>
+        <a :href="baseUrl">Thoughts App</a>
     </div>
 </template>
 
 <script>
 export default {
     props: ['list'],
-    emits: ['openNavbar']
+    emits: ['openNavbar'],
+    data() {
+        return {
+            baseUrl: import.meta.env.BASE_URL
+        }
+    }
 }
 </script>
