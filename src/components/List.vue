1925d99da16508b5f4e537a4e5813c76307baa77
Johnny <jahongirhakimov677@gmail.com>
2025-08-03 14:34:21 +0500

    fix: reading content

--- src/components/List.vue
+++ src/components/List.vue
@@ -2,7 +2,7 @@
     <transition name="fade">
         <div v-if="list" class="list">
             <!-- <transition-group name="fade" tag="ul" class="list"> -->
-                <li v-for="(thought, index) in thoughts" :key="thought.id || index" :class="{ thought: list}" @click="$emit('openThought', thought.id)">
+                <li v-for="(thought, index) in thoughts" :key="thought.id || index" :class="{ thought: list}" @click="$emit('openThought', index)">
                     <span class="title">{{ thought.title }} <span class="createdAt">{{ thought.created_at }}</span></span>
                     <button class="deleteBtn" @click.stop="$emit('removeThought', index)"><i class="bi bi-trash3-fill"></i></button></li>
             <!-- </transition-group> -->
